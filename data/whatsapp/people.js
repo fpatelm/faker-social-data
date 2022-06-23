dd623e92ca17a5cd6f3417938ff70f8722cf1b38
Faizal Patel <faizalmw@hotmail.com>
2022-06-23 20:40:06 +0100

    add list of stories

--- data/whatsapp/people.js
+++ data/whatsapp/people.js
@@ -3,16 +3,24 @@ import {rdn_boolean } from '../common/index.js'
 export function people() {
   let reply = [];
   for (let i = 0; i < faker.random.numeric(2); i++) {
+    let isStory = rdn_boolean();
+    let stories = [];
+    if (isStory) { 
+      for (let i = 0; i < faker.random.numeric(2); i++) {
+        stories.push(faker.image.image());
+      }
+    }
     reply.push({
       firstName: faker.name.firstName(),
       lastName:faker.name.lastName(),
       msg: faker.lorem.sentence(15),
       date: faker.date.month(),
       count: faker.random.numeric(),
-      story: rdn_boolean(),
-      image: faker.image.fashion(),
+      story: isStory,
+      image: faker.image.image(),
       avatar: faker.image.avatar(),
-      status: faker.lorem.words()
+      status: faker.lorem.words(),
+      stories:stories
     });
   }
   return reply;
