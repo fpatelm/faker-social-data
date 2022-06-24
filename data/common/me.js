82ba1dd694b5583dd9a5072871807a47ebdb3b28
Faizal Patel <faizalmw@hotmail.com>
2022-06-24 08:34:47 +0100

    add stories to me endpoint

--- data/common/me.js
+++ data/common/me.js
@@ -2,6 +2,15 @@ import { faker } from '@faker-js/faker';
 import {rdn_boolean } from './index.js'
 
 export default function me() { 
+
+    let isStory = rdn_boolean();
+    let stories = [];
+    if (isStory) { 
+      for (let i = 0; i < faker.random.numeric(2); i++) {
+        stories.push(faker.image.image());
+      }
+    }
+
     return {
         firstname: faker.name.firstName(),
         lastname: faker.name.lastName(),
@@ -11,7 +20,8 @@ export default function me() {
         gender:faker.name.gender(true),
         job_title: faker.name.jobTitle(),
         job_area:faker.name.jobArea(),
-        story: rdn_boolean(),
-        status: faker.lorem.words()
+        story: isStory,
+        status: faker.lorem.words(),
+        stories: stories
     };
 }
